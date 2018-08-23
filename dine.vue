b13eaacc6fd424cddd13e2f4c585785fc505d53c
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-08-23 15:34:21 -0400

    The District at Tustin Legacy 2.0 1.0  /dine.vue

--- dine.vue
+++ dine.vue
@@ -169,7 +169,6 @@
                         }
                     });
                     this.filteredStores = store_list;
-                    console.log("store_list",store_list);
                     return store_list
                 },
                 dropDownCats() {
