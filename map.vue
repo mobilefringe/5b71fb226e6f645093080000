6935121aa5cf1d09794ed0541d96746dd4e0b14b
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-08-29 11:44:54 -0400

    The District at Tustin Legacy 2.0 1.0  /map.vue

--- map.vue
+++ map.vue
@@ -76,11 +76,13 @@
                 this.loadData().then(response => {
                     this.loadData().then(response => {
                     var temp_repo = this.findRepoByName('Map Banner').images;
-                    if(temp_repo != null) {
-                        this.pageBanner = temp_repo[0];
-                    } else {
+                    if(temp_repo !== null && temp_repo !== undefined) {
+                       temp_repo = temp_repo.images;
+                       this.pageBanner = temp_repo[0];
+                    }
+                    else {
                         this.pageBanner = {
-                            "image_url": "//codecloud.cdn.speedyrails.net/sites/5b71e1e76e6f6411c4060000/image/jpeg/1529532304000/insidebanner2.jpg"
+                            "image_url": "//codecloud.cdn.speedyrails.net/sites/5b71eb886e6f6450013c0000/image/jpeg/1529532304000/insidebanner2.jpg"
                         }
                     }
                     
