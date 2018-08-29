f08da2dc8b1dd1e6e4e9f7f5e414287fcf2d2e92
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-08-29 13:16:28 -0400

    The District at Tustin Legacy 2.0 1.0  /coupon_basket.vue

--- coupon_basket.vue
+++ coupon_basket.vue
@@ -29,7 +29,8 @@
         		        </div>
         		    </div>
                     <div class="row">
-                        <div v-for="(item, index) in couponList" :key="item.id" v-on:remove="couponList.splice(index, 1)" class="col-md-6 col-sm-6 col-xs-12">
+                        <div v-for="(item, index) in couponList" :key="item.id" class="col-md-6 col-sm-6 col-xs-12"> 
+                         <!--v-on:remove="couponList.splice(index, 1)"-->
                             <div :id="item.id" class="row coupon_container">
                                 <div class="col-md-6 col-sm-6 col-xs-12">
                                     <div class="coupon_img">
