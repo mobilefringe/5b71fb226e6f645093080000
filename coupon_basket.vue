826efaa71c8c518a5c8c7b1ff22c7301f7fc104f
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-08-29 17:18:15 -0400

    The District at Tustin Legacy 2.0 1.0  /coupon_basket.vue

--- coupon_basket.vue
+++ coupon_basket.vue
@@ -167,7 +167,7 @@
                         var field = null;
                         if(!_.includes(value.promo_image_url_abs, 'missing')){
                             field = value.promo_image_url_abs;
-                             console.log("field", field)
+                             console.log("field", field, value)
                         }
                         else {
                             field = vm.property.default_logo;
