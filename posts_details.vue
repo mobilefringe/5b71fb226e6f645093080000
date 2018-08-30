3e5c6e500ef223cb3b8e0494967390c8389183ae
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-08-30 14:19:01 -0400

    The District at Tustin Legacy 2.0 1.0  /posts_details.vue

--- posts_details.vue
+++ posts_details.vue
@@ -84,7 +84,7 @@
 					if (this.currentPromo === null || this.currentPromo === undefined) {
 						this.$router.replace({ path: '/posts' });
 					}
-					this.$breadcrumbs[2].meta.breadcrumb = this.currentPromo.name
+				// 	this.$breadcrumbs[2].meta.breadcrumb = this.currentPromo.name
 					this.dataLoaded = true;
 				}, error => {
 					console.error("Could not retrieve data from server. Please check internet connection and try again.");
