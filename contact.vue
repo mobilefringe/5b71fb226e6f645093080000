b9ce6e65a494292e02582885204a906e94fd10ba
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-09-17 10:52:41 -0400

    The District at Tustin Legacy 2.0 1.0  /contact.vue

--- contact.vue
+++ contact.vue
@@ -93,7 +93,7 @@
     }
 </style>
 <script>
-    define(["Vue", "vuex", "vue-meta", "vee-validate", "json!site.json"], function (Vue, Vuex, Meta, VeeValidate, site) {
+    define(["Vue", "vuex", "vue-meta", "vee-validate", "json!site.json"], function (Vue, Vuex, Meta, VeeValidate, Site) {
         Vue.use(Meta);
         Vue.use(VeeValidate);
         return Vue.component("contact-component", {
@@ -143,7 +143,7 @@
                 loadData: async function () {
                     this.property.mm_host = this.property.mm_host.replace("http:", "");
                     try {
-                        let results = await Promise.all([this.$store.dispatch('LOAD_PAGE_DATA', { url: this.property.mm_host + "/pages/"+site.subdomain+"-contact-us.json" }), this.$store.dispatch("getData", "repos")]);
+                        let results = await Promise.all([this.$store.dispatch('LOAD_PAGE_DATA', { url: this.property.mm_host + "/pages/"+Site.subdomain+"-contact-us.json" }), this.$store.dispatch("getData", "repos")]);
                         return results;
                     } catch (e) {
                         console.log("Error loading data: " + e.message);
