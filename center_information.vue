81371c0a6dc4f78b571e671e87561253e8c4c0b7
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-09-17 13:35:44 -0400

    The District at Tustin Legacy 2.0 1.0  /center_information.vue

--- center_information.vue
+++ center_information.vue
@@ -101,7 +101,7 @@
                 loadData: async function () {
                     this.property.mm_host = this.property.mm_host.replace("http:", "");
                     try {
-                        let results = await Promise.all([this.$store.dispatch("getData", "repos"), this.$store.dispatch('LOAD_PAGE_DATA', {url: this.property.mm_host + "/pages/district-center-information.json"})]);
+                        let results = await Promise.all([this.$store.dispatch("getData", "repos"), this.$store.dispatch('LOAD_PAGE_DATA', {url: this.property.mm_host + "/pages/"+Site.subdomain+"-center-information.json"})]);
                         return results;
                     } catch (e) {
                         console.log("Error loading data: " + e.message);
