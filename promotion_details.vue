6d4a1b730b3bb341523b4ca2d35fab52ba1248a4
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-11-21 16:53:27 -0500

    The District at Tustin Legacy 2.0 1.0  /promotion_details.vue

--- promotion_details.vue
+++ promotion_details.vue
@@ -5,7 +5,7 @@
             <div v-if="dataLoaded" v-cloak>
                 <div class="inside_page_header" v-if="pageBanner" v-bind:style="{ backgroundImage: 'url(' + pageBanner.image_url + ')' }">
                     <div class="main_container position_relative">
-                        <h2>Sales & Promotions</h2>
+                        <h2>Events & Promotions</h2>
                     </div>
                 </div>
                 <div class="main_container">
